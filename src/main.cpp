4a211874d5ad0a67ffe06dbbebfe7df830da365a
Mehedi Hasan <mehedihasanaurora@gmail.com>
2024-12-23 23:42:58 +0600

    S-4: Implemented exit command

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,19 @@
 #include <string>
 
 int main() {
-    // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
     
-    // Uncomment this block to pass the first stage
-    std::cout << "$ ";
-    std::string input;
-    std::getline(std::cin, input);
-    std::cout << input << ": command not found" << std::endl;
-    
-    //creating REPL
     while(true) {
-    std::cout << "$ ";
-    std::string input;
-    std::getline(std::cin, input);
-    std::cout << input << ": command not found" << std::endl;
-  }
+        std::cout << "$ ";
+        std::string input;
+        std::getline(std::cin, input);
+
+        if (input == "exit 0") 
+            return 0;
+            
+        std::cout << input << ": command not found" << std::endl;
+    }
     
     return 0;
 }
